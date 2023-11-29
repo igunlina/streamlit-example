5450b60d94f34e3879444ada0dff878ec07ec5be
igunlina <152131762+igunlina@users.noreply.github.com>
2023-11-29 15:02:24 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
 def main():
-    st.title('Halaman Depan dengan Gambar')
+    st.title('Halaman Depan dengan Gambar dari GitHub')
     st.header('Selamat datang di Halaman Depan!')
 
-    # Menampilkan gambar dari direktori lokal
-    image = open('Indefinite.jpg', 'rb')
-    st.image(image, caption='Deskripsi Gambar', use_column_width=True)
+    # URL raw dari gambar di repositori GitHub
+    github_image_url = 'https://raw.githubusercontent.com/NamaPenggunaAnda/RepoNamaAnda/NamaBranchAnda/path/ke/gambar.jpg'
+
+    # Menampilkan gambar dari URL
+    st.image(github_image_url, caption='Deskripsi Gambar', use_column_width=True)
 
 if __name__ == '__main__':
     main()
