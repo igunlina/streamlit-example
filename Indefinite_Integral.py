2038a8e54ecb9c5f757453e6a3f21bd8332740e6
igunlina <152131762+igunlina@users.noreply.github.com>
2023-11-29 14:55:28 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -5,7 +5,7 @@ def main():
     st.header('Selamat datang di Halaman Depan!')
 
     # Menampilkan gambar dari URL
-    st.image('https://drive.google.com/file/d/1OtAx1fWYrWeHNaIEsxZpqhEV6JiNzq_a/view?usp=sharing', caption='Deskripsi Gambar', use_column_width=True)
+    st.image('https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fd%2Fd1%2FImproperintegral2.png%2F1200px-Improperintegral2.png&tbnid=nFXqY7Ob63gv1M&vet=12ahUKEwj08o2-3eiCAxU_5TgGHagwB2UQMygDegQIARB0..i&imgrefurl=https%3A%2F%2Fid.wikipedia.org%2Fwiki%2FIntegral_takwajar&docid=uHoWXdZmLEZVBM&w=1200&h=772&q=integral&ved=2ahUKEwj08o2-3eiCAxU_5TgGHagwB2UQMygDegQIARB0', caption='Deskripsi Gambar', use_column_width=True)
 
 if __name__ == '__main__':
     main()
