4fcf5134623939a95107be83688b1e2168f0812e
igunlina <152131762+igunlina@users.noreply.github.com>
2023-11-29 15:14:15 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -10,5 +10,17 @@ def main():
     # Menampilkan gambar dari URL
     st.image(github_image_url, caption='Deskripsi Gambar', use_column_width=True)
 
+ # Menu di sidebar
+    menu = st.sidebar.radio("Menu", ["Beranda", "Tentang", "Kontak"])
+
+    if menu == "Beranda":
+        st.write("Anda berada di Beranda.")
+        # Tambahkan konten Beranda di sini
+    elif menu == "Tentang":
+        st.write("Anda berada di Halaman Tentang.")
+        # Tambahkan konten Tentang di sini
+    elif menu == "Kontak":
+        st.write("Anda berada di Halaman Kontak.")
+        # Tambahkan konten Kontak di sini
 if __name__ == '__main__':
     main()
