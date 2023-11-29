d4a44e3b373d352c590e1b363cd6448ecae889c1
igunlina <152131762+igunlina@users.noreply.github.com>
2023-11-29 15:11:49 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -5,7 +5,7 @@ def main():
     st.header('Selamat datang di Halaman Depan!')
 
     # URL raw dari gambar di repositori GitHub
-    github_image_url = 'https://raw.githubusercontent.com/NamaPenggunaAnda/RepoNamaAnda/NamaBranchAnda/path/ke/gambar.jpg'
+    github_image_url = 'https://raw.githubusercontent.com/igunlina/streamlit-example/master/Indefinite.jpg'
 
     # Menampilkan gambar dari URL
     st.image(github_image_url, caption='Deskripsi Gambar', use_column_width=True)
