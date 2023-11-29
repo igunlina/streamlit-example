c3bc9b8a77ad2818a720940c9931e2b38ab72c5a
igunlina <152131762+igunlina@users.noreply.github.com>
2023-11-29 14:52:22 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
 def main():
-    st.title('Halaman Depan')
+    st.title('INDEFINITE INTEGRAL')
     st.header('Selamat datang di Halaman Depan!')
-    st.write('Ini adalah halaman depan sederhana menggunakan Streamlit.')
+
+    # Menampilkan gambar dari URL
+    st.image('https://drive.google.com/file/d/1OtAx1fWYrWeHNaIEsxZpqhEV6JiNzq_a/view?usp=sharing', caption='Deskripsi Gambar', use_column_width=True)
 
 if __name__ == '__main__':
     main()
