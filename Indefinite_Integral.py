3fb48c75fba42617d6d112856215b6e2774a4d20
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-05 13:25:35 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -7,28 +7,19 @@ def main():
     # URL raw dari gambar di repositori GitHub
     github_image_url = 'https://raw.githubusercontent.com/igunlina/streamlit-example/master/Indefinite.jpg'
 
-    # Menampilkan gambar dari URL
-    st.image(github_image_url, caption='Semoga ilmunya bermanfaat', use_column_width=True)
-
-    # Menambahkan audio dari path lokal (pastikan mengonversi backslashes menjadi double backslashes atau gunakan forward slashes)
-    audio_file_path = 'children-logo-116101.mp3'  # Ganti dengan path file audio Anda
-    st.audio(audio_file_path, format='audio/mp3', start_time=0)
-
-    # Menu di sidebar
+    # Menampilkan gambar dari URL hanya di halaman Beranda
     menu = st.sidebar.radio("Menu", ["Beranda", "Tentang", "Kontak"])
 
     if menu == "Beranda":
-        st.write("Anda berada di Beranda. Ini adalah konten untuk halaman Beranda.")
+        st.image(github_image_url, caption='Semoga ilmunya bermanfaat', use_column_width=True)
         st.subheader("Selamat datang di halaman Beranda!")
         st.write("Halaman ini berisi informasi umum tentang pembelajaran Indefinite Integral.")
         st.write("Silakan jelajahi konten-konten yang tersedia.")
     elif menu == "Tentang":
-        st.write("Anda berada di Halaman Tentang. Ini adalah konten untuk halaman Tentang.")
         st.subheader("Tentang Kami")
         st.write("Kami adalah tim yang berkomitmen untuk memberikan informasi seputar integral.")
         st.write("Kami berharap informasi yang kami sajikan dapat membantu Anda memahami konsep ini dengan lebih baik.")
     elif menu == "Kontak":
-        st.write("Anda berada di Halaman Kontak. Ini adalah konten untuk halaman Kontak.")
         st.subheader("Hubungi Kami")
         st.write("Jika Anda memiliki pertanyaan atau masukan, silakan hubungi kami melalui:")
         st.write("- Email: example@email.com")
