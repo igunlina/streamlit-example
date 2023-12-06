a59e6f0b4cb595ccd54e5c16120199995282ed4c
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-06 10:27:10 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -26,12 +26,11 @@ def main():
         youtube_video_url_review = 'https://www.youtube.com/watch?v=XdiA9E5HC4I'  # Ganti dengan URL video YouTube Anda
         st.video(youtube_video_url_review)
 
-        st.write("Sebelum mempelajari integral, mari kita review materi turunan agar mempelajari integral jadi lebih mudah")
-        st.write("Kami adalah tim yang berkomitmen untuk memberikan informasi seputar integral.")
-        st.write("Kami berharap informasi yang kami sajikan dapat membantu Anda memahami konsep ini dengan lebih baik.")
-        st.write("Dalam video ini, kami membahas beberapa contoh turunan untuk membantu Anda memahami konsep ini secara praktis.")
-
-        st.write("Selamat menonton!")
+        st.write("Sebelum mempelajari integral, mari kita review materi turunan agar mempelajari integral jadi lebih mudah.")
+       
+        st.write("Pastikan materi turunan sudah dikuasi.")
+       
+        st.write("Selamat menyimak!")
     elif menu == "Rumus Dasar Integral":
         st.subheader("Tentang Kami")
         st.write("Kami adalah tim yang berkomitmen untuk memberikan informasi seputar integral.")
