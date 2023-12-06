12d8d3a6fb1e98ae17425c0d02643201cecb50e9
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-06 10:29:50 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -28,7 +28,7 @@ def main():
 
         st.write("Sebelum mempelajari integral, mari kita review materi turunan agar mempelajari integral jadi lebih mudah.")
        
-        st.write("Silahkan simak video pembelajaran turunan di atas dengan baik,  pastikan materi turunan kalian kuasi.")
+        st.write("Silahkan simak video pembelajaran turunan di atas dengan baik")
        
         st.write("Selamat menyimak!")
     elif menu == "Rumus Dasar Integral":
