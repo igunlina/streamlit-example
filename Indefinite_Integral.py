25b98da6e98536ce9914c246d04a226f41518c41
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-06 12:27:23 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -20,7 +20,7 @@ def main():
         st.markdown("(Indefinite Integral /Primitive)")
         st.markdown(r"**Misalkan** $I$ **sebuah interval yang memuat lebih dari satu titik, dan sebarang fungsi** $f:I \to \mathbb{R}$. **Sebuah fungsi yang terdifferensialkan** $F$ **disebut sebagai primitive dari** $f$ **pada interval** $I$, **jika** $F'(x) = f(x)$, $x \in I$.")
 
-        st.write("CONTOH 1.")
+        st.write("<font color="blue"> CONTOH 1."</font>', unsafe_allow_html=True)
         st.markdown('<font color="red">**Fungsi** $f(x)=2x$ **memiliki primitive** $F(x)=x^2$ pada   $ \mathbb{R}$  **sehingga** $(x^2)\'=2x$ </font>', unsafe_allow_html=True)
 
         st.write("CONTOH 2.")
