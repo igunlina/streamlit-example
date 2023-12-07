a4d61a142a20809a857f614834cf3edc953d2604
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-07 10:34:13 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -108,13 +108,13 @@ def run_definisi_quiz():
 
 def run_rumus_dasar_quiz():
     # Pertanyaan 1
-    answer_1 = st.radio(" $\int_{}^{}{5} da = $", ["$F(x)=2x^2$", "$F(x)=4x^2$", "$F(x)=4$"])
+    answer_1 = st.radio(" $\int_{}^{}{5} da = $", ["$5a +C$", "$5x+C$", "$5ax+C$"])
 
     # Pertanyaan 2
-    answer_2 = st.radio("Tentukan primitive dari fungsi $f(x)=x^2$", ["$F(x)= \\frac{1}{3} x^3$", "$F(x)=2x$", "$F(x)=\\frac{1}{2} x^2$"])
+    answer_2 = st.radio(" $\int_{}^{}{m^3}dm=$", ["$\\frac{1}{4} m^4 +C $", "$\\frac{1}{3} m^3 +C $", "$\\frac{1}{4} x^4 +C$"])
 
     # Pertanyaan 3
-    answer_3 = st.radio("Tentukan primitive dari fungsi $f(x)=6$", ["$F(x)= 6x$", "$F(x)=0$", "$F(x)=3x^2$"])
+    answer_3 = st.radio(" $\int_{}^{}{ \frac{1}{x^2} dx=}$", ["$- \frac{1}{x} +C$", "$- \\frac{2}{x} +C$", "$- \\frac{1}{x^2} +C$"])
 
     # Tombol Submit
     submitted = st.button("Submit")
@@ -135,7 +135,7 @@ def check_definisi_answers(answer_1, answer_2, answer_3):
 
 def check_rumus_dasar_answers(answer_1, answer_2, answer_3):
     # Logika pengecekan jawaban dan memberikan umpan balik
-    correct_answers = {"$F(x)=2x^2$": "Benar", "$F(x)= \\frac{1}{3} x^3$": "Benar", "$F(x)= 6x$": "Benar"}
+    correct_answers = {"$5a +C$": "Benar", "$\\frac{1}{4} m^4 +C$": "Benar", "$- \frac{1}{x} +C$": "Benar"}
 
     if answer_1 in correct_answers and answer_2 in correct_answers and answer_3 in correct_answers:
         st.success("Selamat! Jawaban Anda benar.")
