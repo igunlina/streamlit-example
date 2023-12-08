1d99a58e539bc00945e2c74ee9dee58d82d7f7ab
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-08 14:48:53 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -36,26 +36,7 @@ def main():
         if st.button("Kuis Definisi Integral"):
             run_definisi_quiz()
               
-        # Pertanyaan 1
-        answer_1 = st.radio("Tentukan primitive dari fungsi $f(x)=2x$", ["$F(x)=x^2$", "$F(x)=2x^2$", "$F(x)=4$"]) 
-        # Pertanyaan 2
-        answer_2 = st.radio("Tentukan primitive dari fungsi $f(x)=x^2$", ["$F(x)= \\frac{1}{3} x^3$", "$F(x)=2x$", "$F(x)=\\frac{1}{2} x^2$"])
-
-        # Pertanyaan 3
-        answer_3 = st.radio("Tentukan primitive dari fungsi $f(x)=0$", ["$F(x)=0$", "$F(x)=C \in \mathbb{R}$", "$F(x)=3x^2$"])
-        # Tombol Kuis
-        if st.button("submit"):
-            check_definisi_answers(answer_1, answer_2, answer_3)
-            
-def check_definisi_answers(answer_1, answer_2, answer_3):
-    # Logika pengecekan jawaban dan memberikan umpan balik
-    correct_answers = {"$F(x)=x^2$": "Benar", "$F(x)= \\frac{1}{3} x^3$": "Benar", "$F(x)=0$": "Benar"}
-
-    if answer_1 in correct_answers and answer_2 in correct_answers and answer_3 in correct_answers:
-        st.success("Selamat! Jawaban Anda benar.")
-    else:
-        st.error("Mohon maaf, jawaban Anda salah. Silahkan diulangi kembali.")
-   
+       
     elif menu == "Review Turunan":
         st.subheader("Review Turunan")
 
