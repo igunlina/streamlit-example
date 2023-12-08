b0c212fad403ad3467a58597941d80ca4108f9a6
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-08 14:55:11 +0700

    Update Indefinite_Integral.py

--- Indefinite_Integral.py
+++ Indefinite_Integral.py
@@ -175,7 +175,7 @@ def main():
     elif menu == "Teknik Integrasi":
         
         # URL raw dari gambar di repositori GitHub
-        github_image_url = 'https://raw.githubusercontent.com/igunlina/streamlit-example/master/Sifat%20kelinieran%20integral.jpg'
+        github_image_url = 'https://raw.githubusercontent.com/igunlina/streamlit-example/master/SUBSTITUSI.jpg'
         st.image(github_image_url, caption='Semoga ilmunya bermanfaat', use_column_width=True)   
         
         # Menambahkan audio dari path lokal (pastikan mengonversi backslashes menjadi double backslashes atau gunakan forward slashes)
@@ -223,7 +223,10 @@ def main():
             \end{align*}
 
          ''')
-    
+        # URL raw dari gambar di repositori GitHub
+        github_image_url = 'https://raw.githubusercontent.com/igunlina/streamlit-example/master/SUBSTITUSI.jpg'
+        st.image(github_image_url, caption='Semoga ilmunya bermanfaat', use_column_width=True)
+        
         # Tombol Kuis
         if st.button("Kuis Sifat Kelinieran Integral"):
             run_sifat_kelinieran_quiz()
